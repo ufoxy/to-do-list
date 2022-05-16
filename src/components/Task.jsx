e98383de1ac88d1cbad8b8660036313f7fb71da9
foxyuh <arthurbryansk@gmail.com>
2022-05-16 17:45:52 -0300

    iniciando projeto do curso

---
+++ src/components/Task.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+import "./Task.css"
+
+export default function Task({ task }) {
+
+    return (
+        <div className="task-container">
+            {task.title}
+        </div>
+    );
+};
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,16 @@
+import react from 'react';
+import Task from './Task.jsx'
+
+export default function Tasks({ tasks }) {
+
+    console.log(tasks)
+    console.log('oi')
+
+    return (
+        <>
+            {tasks.map((task) => (
+            <Task task={task}/>
+            ))}
+        </>
+    )
+}
