7f4166dfa2aa3d365c284be07a0d8c3358fc8466
foxyuh <arthurbryansk@gmail.com>
2022-05-18 16:02:12 -0300

    personalização do button

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 import Button from './Button';
 import './TaskDetails.css';
 
 function TaskDetails() {
-    const params = useParams()
+    const params = useParams();
+    const history = useHistory();
 
-    console.log(params)
+    function handleBackButtonClick() {
+        history.goBack();
+    };
     
     return (
         <>
             <div className='back-button-container'>
-                <Button children={"Voltar"} />
+                <Button 
+                children={"Voltar"}
+                onClick={handleBackButtonClick}
+                 />
             </div>
             <div className="task-details-container">
                 <h2>{params.taskTitle}</h2>
